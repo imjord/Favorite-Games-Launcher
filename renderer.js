af87bd9beb7f2131ecbc72d85e837563ac6c4e5a
imjord <dean.jay.goat@gmail.com>
2023-03-22 22:50:09 -0600

    scrapped trying to make light/dark mode because it kept changing back once a li was added.

---
+++ renderer.js
@@ -0,0 +1,18 @@
+// let darkModeEl = document.querySelector(".mode");
+// let mainBackground = document.querySelector(".games-wrapper");
+// let nav = document.querySelector("#nav");
+// let btns = document.querySelectorAll("button");
+// let li = document.querySelectorAll("li");
+
+// darkModeEl.addEventListener("click", () => {
+//   darkModeEl.style.backgroundColor = "white";
+//   mainBackground.style.backgroundColor = "white";
+//   nav.style.backgroundColor = "lightgray";
+//   btns.forEach((btn) => {
+//     btn.style.backgroundColor = "gray";
+//     btn.style.color = "white";
+//   });
+//   li.forEach((li) => {
+//     li.style.color = "black";
+//   });
+// });
